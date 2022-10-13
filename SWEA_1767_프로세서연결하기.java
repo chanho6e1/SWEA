74d04358bdb51e7bd4958a11601eae66bf1ce995
chanho6e1 <108286046+chanho6e1@users.noreply.github.com>
2022-10-14 01:20:17 +0900

    Update SWEA_1767_프로세서연결하기.java

--- SWEA_1767_프로세서연결하기.java
+++ SWEA_1767_프로세서연결하기.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
-
+ 
 public class SWEA_1767_프로세서연결하기 {
 	
 	static int N, map[][];
