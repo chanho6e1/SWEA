8b0a9b1241ecdb0034245f4ba2f87e285af7adc6
chanho6e1 <108286046+chanho6e1@users.noreply.github.com>
2022-10-14 01:20:53 +0900

    Update SWEA_5656_벽돌깨기.java

--- SWEA_5656_벽돌깨기.java
+++ SWEA_5656_벽돌깨기.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
-
+ 
 public class SWEA_5656_벽돌깨기 {
 
 	static int N, W, H, res;
