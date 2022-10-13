9bb7adbbe3d1f06b9c6085a33a9d695b7b89be53
chanho6e1 <108286046+chanho6e1@users.noreply.github.com>
2022-10-14 01:20:36 +0900

    Update SWEA_4012_요리사.java

--- SWEA_4012_요리사.java
+++ SWEA_4012_요리사.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader; 
 import java.util.StringTokenizer;
-
+ 
 public class SWEA_4012_요리사 {
 	
 	static int n, r, map[][], min;
